e2a31b70ae9acf4777a71bc26e28c34804da94bb
haedoang <haedoang@gmail.com>
2020-01-07 00:12:24 +0900

    Updates

---
+++ precache-manifest.f52019a6bbfeb9b39c9d734f2e1bce40.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0a328e4d3706c12bc2ec16b5b7662e25",
+    "url": "/movie_app/index.html"
+  },
+  {
+    "revision": "2fc944000c909ed02248",
+    "url": "/movie_app/static/css/main.3e7a1008.chunk.css"
+  },
+  {
+    "revision": "6dd822191f3d167b03d1",
+    "url": "/movie_app/static/js/2.c4df8893.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/movie_app/static/js/2.c4df8893.chunk.js.LICENSE"
+  },
+  {
+    "revision": "2fc944000c909ed02248",
+    "url": "/movie_app/static/js/main.6095bd53.chunk.js"
+  },
+  {
+    "revision": "f76e6ea8385f6c19ad9c",
+    "url": "/movie_app/static/js/runtime-main.1e1f7430.js"
+  }
+]);
